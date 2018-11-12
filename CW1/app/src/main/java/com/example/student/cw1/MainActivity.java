cdc00b8c8754acc2400819aa32cab6a3c90de3db
Lotus <cmadisonne@gmail.com>
2018-11-12 13:42:56 -0600

    Attempt

--- CW1/app/src/main/java/com/example/student/cw1/MainActivity.java
+++ CW1/app/src/main/java/com/example/student/cw1/MainActivity.java
@@ -2,27 +2,34 @@ package com.example.student.cw1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText userInput;
+    TextView textView;
+    Integer numTimesClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numTimesClicked = numTimesClicked + 1;
+                String result = "\nClicked " + numTimesClicked + " times";
+                textView.append(result);
 
-    var userInput = (EditText) findViewById(R.id.editText);
-    var button = (Button) findViewById(R.id.button);
-    var textView = (TextView) findViewById(R.id.textView);
-    Integer numTimesClicked;
-    View.OnClickListener('ourOnClickListener') = new View.OnClickListener() {
-        @Override
-        public void onClick(View.view) {
-            numTimesClicked = numTimesClicked + 1;
-            String result = "\nClicked " + numTimesClicked + " times";
-            textView.append(result);
-
-        }
-    };
+            }
+        };
         button.setOnClickListener(ourOnClickListener);
+    }
 }
